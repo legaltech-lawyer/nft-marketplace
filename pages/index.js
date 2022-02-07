076b9c73bfc1e56b1ff015982b2fc265bf2cfff0
Ignacio Bermeo <ibjazz@users.noreply.github.com>
2022-02-07 10:37:46 -0600

    Metamask authentication

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import { useWeb3 } from '@3rdweb/hooks'
+import { useEffect } from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import client from '../lib/sanityClient'
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('https://lh3.googleusercontent.com/soR6JAuB0k-X3Az9G8-NJG9Cfoc4kyfgPUCYVtp6Ker9QQSHg9UQA52eFduoL_v_E5dHnnaB3LtZjBIAZdH6p5e2rLOm7aVm6eVoMg=s550')] before:bg-cover before:bg-center before:opacity-30 before:blur`,
+  button: `relative text-lg font-semibold px-12 py-4 bg-[#2181e2] rounded-lg mr-5 text-white hover:bg-[#42a0ff] cursor-pointer`,
+  details: `text-lg text-center text-white font-semibold mt-4`,
+}
+
+export default function Home() {
+  const { address, connectWallet } = useWeb3()
+
+  // useEffect(() => {
+  //   if (!address) return
+  //   ;(async () => {
+  //     const userDoc = {
+  //       _type: 'users',
+  //       _id: address,
+  //       userName: 'Unnamed',
+  //       walletAddress: address,
+  //     }
+  //     //const result = await client.createIfNotExists(userDoc)
+  //   })()
+  // }, [address])
+
+  return (
+    <div className={style.wrapper}>
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <div className={style.walletConnectWrapper}>
+          <button
+            className={style.button}
+            onClick={() => connectWallet('injected')}
+          >
+            Conectar
+          </button>
+          <div className={style.details}>
+            Necesitas Chrome para conectarte a Metamask
+            <br />y acceder al registro de activos digitales
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
